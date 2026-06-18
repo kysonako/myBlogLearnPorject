a1ab11da0d4fc48e0eacc4529df43a2c2d45f07b
kysonako <serg2russ@yandex.ru>
2026-06-18 14:26:10 +0500

    error solved

--- database/migrations/2026_06_16_093545_create_comments_table.php
+++ database/migrations/2026_06_16_093545_create_comments_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Post;
+use App\Models\User;
 
 return new class extends Migration
 {
@@ -13,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->text('body');
+            $table->foreignIdFor(User::class,'user_id');
+            $table->foreignIdFor(Post::class,'post_id');
+
             $table->timestamps();
         });
     }
