5c292d98bc87e30fe8c134e8bbbcc2378fa0b16a
kysonako <serg2russ@yandex.ru>
2026-06-23 11:45:54 +0500

    Changed login form

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
